5e6ec148b797d93b08148a1021f60f62758d0a9f
John Smith <js@Johns-MBP.home>
2022-05-20 14:43:57 -0400

    simplified the logic

--- forth.c
+++ forth.c
@@ -4,43 +4,41 @@
 // Main Memory
 char memory[100000];
 char *mem = memory;
-
-#define ALLOC(x)						\
-	mem;							\
-	mem += sizeof(x);					\
-
-#define NEXT							\
-	(*ip)++;						\
-	goto ***(ip);						\
-
+		
+#define ALLOC(x)			\
+		mem;			\
+		mem += sizeof(x);	\
+		
+#define NEXT 				\
+		(*ip)++;		\
+		goto ***(ip);		\
+		
 int main() {
 	
-	/* Memory Structures */
-
+	// Instruction Pointer
 	void	***ip	= (void ***)	ALLOC(void **);
 	
+	// Stack
 	long	**sp 	= (long **)	ALLOC(long *);
 		*sp 	= (long *)	ALLOC(long[10]); 
-
+	
+	// Return Stack
 	void	***rsp 	= (void ***)	ALLOC(void **);
 		*rsp 	= (void **)	ALLOC(void *[10]);
-
+	
+	// Dictonary Stack
 	void	***dsp 	= (void ***)	ALLOC(void **);
 		*dsp 	= (void **)	ALLOC(void * [10]);
-
-	/* Memory */
 	
+	/* Memory */
 	void *word[] = { &&lookup, &&bye };
 	*ip = (word)-1;
-	
 	NEXT;
-	
-	return 0;
 	
 	lookup:
 		puts("Run Code");
 		NEXT;
-
+	
 	bye:
 		puts("bye");
 		return 0;
