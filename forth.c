0ab586196018b84896ea0477da55d6782ca88cb2
John Smith <js@Johns-MBP.home>
2022-05-20 13:48:10 -0400

    Hugely simplified the logic

--- forth.c
+++ forth.c
@@ -5,75 +5,43 @@
 char memory[100000];
 char *mem = memory;
 
-// Dictionary entry type
-typedef struct {
-char name[100];
-void* code;
-} dict_t;
-
 #define ALLOC(x)						\
 	mem;							\
-	memcpy(mem, (char*)&(x), sizeof(x));			\
 	mem += sizeof(x);					\
-
-#define DEFINE(x,y)						\
-	(*dsp);							\
-	strcpy((*dsp)->name, x);				\
-	(*dsp)->code = y;					\
-	(*dsp)++;						\
 
 #define NEXT							\
 	(*ip)++;						\
 	goto ***(ip);						\
 
 int main() {
+	
+	/* Memory Structures */
 
-	/* Pointers */
+	void	***ip	= (void ***)	ALLOC(void **);
+	
+	long	**sp 	= (long **)	ALLOC(long *);
+		*sp 	= (long *)	ALLOC(long[10]); 
 
-	void **instruction_pointer;
-	void ***ip = (void ***)ALLOC(instruction_pointer);
-	
-	long *stack_pointer;
-	long **sp = (long **)ALLOC(stack_pointer);
-	
-	void **return_stack_pointer;
-	void ***rsp = (void ***)ALLOC(return_stack_pointer);
-	
-	dict_t *dictionary_stack_pointer;
-	dict_t **dsp = (dict_t **)ALLOC(dictionary_stack_pointer);
-	
-	/* Stacks */
-	
-	long stack[10];
-	*sp = (long *)ALLOC(stack);
+	void	***rsp 	= (void ***)	ALLOC(void **);
+		*rsp 	= (void **)	ALLOC(void *[10]);
 
-	void *return_stack[10];
-	*rsp = (void **)ALLOC(return_stack);
-	
-	dict_t dictionary_stack[10];
-	*dsp = (dict_t *)ALLOC(dictionary_stack);
-	
+	void	***dsp 	= (void ***)	ALLOC(void **);
+		*dsp 	= (void **)	ALLOC(void * [10]);
+
 	/* Memory */
-	char code[] = "lookup";
-	char *cp = code;
 	
-	dict_t *bye = (dict_t *)DEFINE("bye", &&bye);
-	dict_t *lookup = (dict_t *)DEFINE("lookup", &&lookup);
-	
-	void *word[] = { lookup->code, bye->code };
+	void *word[] = { &&lookup, &&bye };
 	*ip = (word)-1;
-
+	
 	NEXT;
 	
 	return 0;
 	
-	bye:
-		puts("bye");
-		return 0;
-		
 	lookup:
 		puts("Run Code");
 		NEXT;
-}
 
-// { hidden macro 1 1 + } <- test
+	bye:
+		puts("bye");
+		return 0;
+}
