4a417c13836a13578bf0c9c7338c7fba35f4651c
173duprot <173duprot@protonmail.com>
2022-08-05 01:42:41 -0400

    Indirect Threading

--- forth.c
+++ forth.c
@@ -1,62 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-// ha
-
 // Main Memory
 char memory[100000];
 char *mem = memory;
 		
-#define ALLOC(x)			\
-		mem;			\
-		mem += sizeof(x);	\
-
-#define NEXT 				\
-		(*ip)=(*nip);		\
-		(*nip)++;		\
-		goto **(*ip);		\
-
-#define PUSH(x,sp)	*(sp) = (x); (sp)++;
-#define POP(sp)		*((sp)--);
+#define ALLOC(x) 	mem; mem += sizeof(x);
+#define NEXT 		(*ip)++; goto **(*ip-1);
 		
 int main() {
-	
-	// Instruction Pointer
-	void	****ip	= (void ****)	ALLOC(void ***);
-	void	****nip	= (void ****)	ALLOC(void ***);
-	
-	// Stack
-	long	**sp 	= (long **)	ALLOC(long *);
-		*sp 	= (long *)	ALLOC(long[10]); 
-	
-	// Return Stack
-	void	***rsp 	= (void ***)	ALLOC(void **);
-		*rsp 	= (void **)	ALLOC(void *[10]);
-	
-	// Dictonary Stack
-	void	***dsp 	= (void ***)	ALLOC(void **);
-		*dsp 	= (void **)	ALLOC(void * [10]);
-	
+
 	/* Memory */
-	void *bye		= &&bye;
-	void *code		= &&code;
-	void *exit		= &&exit;
-	void **subword[]	= { &&docol, &code, &exit };
-	void **word[]		= { &&docol, &subword, &bye };
-	*nip = word;
+	void  ***ip = (void ***) ALLOC(void **);     // Instruction Pointer
+
+	long   **sp = (long **)	 ALLOC(long *);	     // Stack Pointer
+	        *sp = (long *)	 ALLOC(long[10]);    // Stack
+
+	void ***rsp = (void ***) ALLOC(void **);     // Return Stack Pointer
+	       *rsp = (void **)	 ALLOC(void *[10]);  // Return Stack
+
+	void ***dsp = (void ***) ALLOC(void **);     // Dictonary Stack Pointer
+	       *dsp = (void **)  ALLOC(void * [10]); // Dictionary Stack
+	
+	/* Code */
+	void *test[] = { &&code, &&retrn };
+	void *word[] = { &&call, &test, &&code, &&bye};
+	*ip = (word);
 	NEXT;
 	
-	docol:
-		PUSH(*nip, *rsp);
-
-		/* set nip to the next thing and run it */
-		(*nip)	= *(*ip + 1); 
-		NEXT;
-
-	exit:
-		*nip = POP(*rsp);
+	call:
+		puts("Call");
+		**rsp = *ip+1; (*rsp)++; // Push ip+1 onto return stack
+		*ip = (**ip);
 		NEXT;
 	
+	retrn:
+		puts("Return");
+		(*rsp)--; *ip = **rsp;
+		NEXT;
+
 	code:
 		puts("Run Code");
 		NEXT;
