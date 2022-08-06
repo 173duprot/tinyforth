9c55fc85207bc17ff5ea99d1ea33528c2da722b7
173duprot <173duprot@protonmail.com>
2022-08-06 02:41:24 -0400

    Indirect threading really working now

--- forth.c
+++ forth.c
@@ -6,44 +6,48 @@ char memory[100000];
 char *mem = memory;
 		
 #define ALLOC(x) 	mem; mem += sizeof(x);
-#define NEXT 		(*ip)++; goto **(*ip-1);
+#define NEXT		(*ip)++; goto ****ip;
+#define POP(x,ptr)	(ptr)--; x=*(ptr);
+#define PUSH(x,ptr)	*(ptr)=x; (ptr)++;
 		
 int main() {
 
 	/* Memory */
-	void  ***ip = (void ***) ALLOC(void **);     // Instruction Pointer
+	void  ****ip = (void ****) ALLOC(void **);     // Instruction Pointer
 
 	long   **sp = (long **)	 ALLOC(long *);	     // Stack Pointer
 	        *sp = (long *)	 ALLOC(long[10]);    // Stack
 
-	void ***rsp = (void ***) ALLOC(void **);     // Return Stack Pointer
-	       *rsp = (void **)	 ALLOC(void *[10]);  // Return Stack
+	void ****rsp = (void ****)ALLOC(void **);     // Return Stack Pointer
+	       *rsp = (void ***) ALLOC(void *[10]);  // Return Stack
 
 	void ***dsp = (void ***) ALLOC(void **);     // Dictonary Stack Pointer
 	       *dsp = (void **)  ALLOC(void * [10]); // Dictionary Stack
 	
 	/* Code */
-	void *test[] = { &&code, &&retrn };
-	void *word[] = { &&call, &test, &&code, &&bye};
-	*ip = (word);
-	NEXT;
+	void *bye = &&bye;
+	void *ret = &&ret;
+	void *code = &&code;
+
+	void **test[] = { &&call, &code, &ret };
+	void ***word[] = { &test, &bye };
+
+	*ip = word;
+	goto ****ip;
 	
-	call:
-		puts("Call");
-		**rsp = *ip+1; (*rsp)++; // Push ip+1 onto return stack
+	call:	puts("Call");
+		PUSH(*ip,*rsp);
 		*ip = (**ip);
 		NEXT;
-	
-	retrn:
-		puts("Return");
-		(*rsp)--; *ip = **rsp;
-		NEXT;
 
-	code:
-		puts("Run Code");
+
+	ret:	puts("Ret");
+		POP(*ip,*rsp);
 		NEXT;
 	
-	bye:
-		puts("bye");
+	code:	puts("Run Code");
+		NEXT;
+	
+	bye:	puts("bye");
 		return 0;
 }
