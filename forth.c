3db5ed252cda905f9f2fe1667c6a4cf4592d9e12
173duprot <173duprot@protonmail.com>
2022-08-09 11:46:25 -0400

    simplified logic

--- forth.c
+++ forth.c
@@ -6,27 +6,17 @@ char memory[100000];
 char *mem = memory;
 		
 #define ALLOC(x) 	mem; mem += sizeof(x);
-#define NEXT		(*ip)++; goto ****ip;
-#define POP(x,ptr)	(ptr)--; x = *(ptr);
-#define PUSH(x,ptr)	*(ptr) = x; (ptr)++;
+#define NEXT		ip++; goto ***ip;
 		
 int main() {
 
 	/* Memory */
 	
-	void  ****ip = (void ****) ALLOC(void **);     // Instruction Pointer
-	
-	char    **cp = (char **)   ALLOC(char *);      // Character Pointer
-	         *cp = (char *)    ALLOC(char [80]);   // Input Buffer
-	
-	long    **sp = (long **)   ALLOC(long *);      // Stack Pointer
-	         *sp = (long *)	   ALLOC(long[10]);    // Stack
-	
-	void ****rsp = (void ****) ALLOC(void **);     // Return Stack Pointer
-	        *rsp = (void ***)  ALLOC(void *[10]);  // Return Stack
-	
-	void  ***dsp = (void ***)  ALLOC(void **);     // Dictonary Stack Pointer
-	        *dsp = (void **)   ALLOC(void * [10]); // Dictionary Stack
+	void  ***ip;                      // Instruction Pointer
+	char    *cp = ALLOC(char [80]);   // Input Buffer
+	long    *sp = ALLOC(long [10]);   // Stack
+	void ***rsp = ALLOC(void *[10]);  // Return Stack
+	void  **dsp = ALLOC(void *[10]);  // Dictionary Stack
 	
 	/* Code */
 	void *lookup	= && lookup;
@@ -38,20 +28,18 @@ int main() {
 	void **test[]	= { call, &code, &retrn };
 	void ***word[]	= { &test, &bye };
 	
-	*ip = word;
-	goto ****ip;
+	ip = word;
+	goto ***ip;
 	
 	lookup: puts("lookup");
 	
-	
-	
 	call:	puts("call");
-		PUSH(*ip,*rsp);
-		*ip = (**ip);
+		*rsp = ip; rsp++;
+		ip = *ip;
 		NEXT;
 	
 	retrn:	puts("return");
-		POP(*ip,*rsp);
+		rsp--; ip = *rsp;
 		NEXT;
 	
 	code:	puts("run code");
