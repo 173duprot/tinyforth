d35015f90bf98d55323912a0a4aae0ecf028c756
173duprot <173duprot@protonmail.com>
2022-08-13 20:35:42 -0400

    workin

--- forth.c
+++ forth.c
@@ -7,44 +7,61 @@ char *mem = memory;
 		
 #define ALLOC(x) 	mem; mem += sizeof(x);
 #define NEXT		ip++; goto ***ip;
+#define PUSH(ptr,x)     *ptr = x; ptr++;
+#define POP(ptr,x)      ptr--; x = *ptr;
 		
 int main() {
-
+	
 	/* Memory */
+	void ***      ip = ALLOC(void **[20]);  // Instruction Pointer
+	char *        in = ALLOC(char   [80]);  // Input Buffer
+	long *     stack = ALLOC(long   [10]);  // Stack
+	void ***  rstack = ALLOC(void  *[10]);  // Return Stack
+	void **     dict = ALLOC(void  *[10]);  // Dictionary Stack
 	
-	void  ***ip;                      // Instruction Pointer
-	char    *cp = ALLOC(char [80]);   // Input Buffer
-	long    *sp = ALLOC(long [10]);   // Stack
-	void ***rsp = ALLOC(void *[10]);  // Return Stack
-	void  **dsp = ALLOC(void *[10]);  // Dictionary Stack
+	/* Core Words*/
+	void *      call = && call;
+	void *     retrn = && retrn;
+	void *       bye = && bye;
+	void *     shell = && shell;
+	void *    lookup = && lookup;
 	
-	/* Code */
-	void *lookup	= && lookup;
-	void *call	= && call;
-	void *retrn	= && retrn;
-	void *bye	= && bye;
-	void *code	= && code;
+	/* Secondary Words */
+	void **end[]	= { call, &lookup };
+	void **test[]	= { call, &shell, &retrn };
 	
-	void **test[]	= { call, &code, &retrn };
-	void ***word[]	= { &test, &bye };
+	/* Filling The Dictionary */
+	PUSH(dict, test);
 	
-	ip = word;
+	/* Run */
+	PUSH(rstack, ip);
+	 PUSH(ip, &test);
+	 PUSH(ip, &bye);
+	POP(rstack, ip);
 	goto ***ip;
 	
-	lookup: puts("lookup");
 	
 	call:	puts("call");
-		*rsp = ip; rsp++;
+		PUSH(rstack, ip);
 		ip = *ip;
 		NEXT;
 	
 	retrn:	puts("return");
-		rsp--; ip = *rsp;
-		NEXT;
-	
-	code:	puts("run code");
+		POP(rstack, ip);
 		NEXT;
 	
 	bye:	puts("bye");
 		return 0;
+
+	shell:	putchar('>');
+		PUSH(rstack, in);
+		do {
+			in++;
+			*in = getchar();
+		} while(*in != '\n');
+		POP(rstack, in);
+		NEXT;
+
+	lookup:	puts("lookup");
+		NEXT;
 }
