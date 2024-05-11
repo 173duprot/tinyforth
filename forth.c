6d94c24d68d6ce44fc2c56be63bdd7d262ba3901
Puppy <pup@system.hampshire.edu>
2024-05-11 12:35:35 -0500

    tes

--- forth.c
+++ forth.c
@@ -1,6 +1,4 @@
-#pragma GCC diagnostic ignored "-Wno-incompatible-pointer-types"
-#pragma GCC diagnostic ignored "-Wno-deprecated-declarations"
-
+//----------------------------------------------------------------------------//
 #include <stdio.h>
 #include <string.h>
 
@@ -8,20 +6,20 @@
 char xxxx[100000];
 char *mem = xxxx;
 		
-#define NEXT		goto ***(++ip)
+
+#define NEXT(xx)	goto ***(xx ip)
 #define ALLOC(x) 	((mem+=sizeof(x))-sizeof(x))
 #define DEFINE(x)	*(dict--) = x
 #define PUSH(ptr,x)     *(ptr++)  = x
 #define POP(ptr)        *(--ptr)
-#define LOG(str)	fputs(str, stderr);
 		
 int main() {
 	
 	/* Memory */
-	void ***  ip; // Instruction Pointer
-	void **** dict   = ALLOC(void ***[100]); dict += 100; // Dictionary
-	long *    stack  = ALLOC(long    [10]);  // Stack
-	void ***  rstack = ALLOC(void   *[10]);  // Return Stack
+	void ***  ip;					// Instruction Ptr
+	void **** dict   = ALLOC(void ***[100]); dict += 100;	// Dictionary
+	long *    stack  = ALLOC(long    [10]);			// Stack
+	void ***  rstack = ALLOC(void   *[10]);			// Return Stack
 	
 	/* Core Words*/
 	void *      call = && call;
@@ -41,33 +39,33 @@ int main() {
 	ip = &test;
 	goto ***ip;
 
-	call:	puts("-> call");		// Call -> Creates local scope.
+	call:	puts("-> call");	// Call -> Creates local scope.
 		PUSH(rstack, ip);
 		ip = *ip;
-		NEXT;
+		NEXT(++);
 	
 	retrn:	puts("-> return");	// Retrn -> Exists local scope.
 		ip = POP(rstack);
-		NEXT;
+		NEXT(++);
 	
 	quit:	puts("-> quit");	// Quit -> Kills program
 		return 0;
 	
 	def:   puts("-> def");
 	       DEFINE(POP(stack));
-	       NEXT;
+	       NEXT(++);
 	
 	parse: puts("-> parse");
 	       ip = dict;
-	       NEXT;
+	       NEXT(++);
 
 	input: puts("-> input");
 	       gets(mem);
 	       PUSH(stack, ALLOC( strlen(mem)+1 ) );
-	       NEXT;
+	       NEXT(++);
 
 	print: puts("-> print");
 	       puts(POP(stack));
-	       NEXT;
+	       NEXT(++);
 	
 }
